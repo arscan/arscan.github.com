09446ece8e0dd1059d05f866f9ff39576d04b65f
Rob Scanlon <robscanlon@gmail.com>
2014-11-07 06:40:42 -0500

    updates to lineup

--- tmp/projection-test/js/SharePanel.js
+++ tmp/projection-test/js/SharePanel.js
@@ -87,7 +87,7 @@ function createSharePanel(renderer, scale){
         renderCamera = new THREE.OrthographicCamera(0, width, height, 0, -1000, 1000),
         renderScene = new THREE.Scene();
 
-        var twitterIconTexture = THREE.ImageUtils.loadTexture('../images/twitter.png', undefined, LOADSYNC.register() );
+        var twitterIconTexture = THREE.ImageUtils.loadTexture('../projection-test/images/twitter.png', undefined, LOADSYNC.register() );
         var twitterIconMaterial = new THREE.MeshBasicMaterial({map: twitterIconTexture, transparent: true});
         var twitterIconGeometry = new THREE.PlaneBufferGeometry( 24 * scale, 19*scale );
         var twitterIconPlane = new THREE.Mesh(twitterIconGeometry, twitterIconMaterial );
@@ -100,7 +100,7 @@ function createSharePanel(renderer, scale){
         twitterPlane.position.set(24*scale*1.6 + width/4 , 5, 0);
         renderScene.add( twitterPlane );
 
-        var githubIconTexture = THREE.ImageUtils.loadTexture('../images/github.png', undefined, LOADSYNC.register() );
+        var githubIconTexture = THREE.ImageUtils.loadTexture('../projection-test/images/github.png', undefined, LOADSYNC.register() );
         var githubIconMaterial = new THREE.MeshBasicMaterial({map: githubIconTexture, transparent: true});
         var githubIconGeometry = new THREE.PlaneBufferGeometry( 25 * scale, 25 * scale);
         var githubIconPlane = new THREE.Mesh(githubIconGeometry, githubIconMaterial );
