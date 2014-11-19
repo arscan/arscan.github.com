9bb863bd5e302e36d717f7e489366db819f84760
Rob Scanlon <robscanlon@gmail.com>
2014-11-19 18:10:25 -0500

    update

--- tmp/projection-test/js/main.js
+++ tmp/projection-test/js/main.js
@@ -26,7 +26,7 @@ function main(renderWidth){
         linksPanel = createLinksPanel(renderer, screenScale),
         backgroundPanel = createBackgroundPanel(renderer, renderWidth, renderHeight),
         projectorPanel = createProjectorPanel(renderer, renderWidth, renderHeight, [namePanel, skeletonPanel, sharePanel, photosPanel, projectsPanel, aboutPanel, bioPanel, linksPanel]),
-        // subjectPanel = createSubjectPanel(renderer, screenScale);//326, 580, 500 + 326/2, 580/2 - 120 ),
+        subjectPanel = createSubjectPanel(renderer, screenScale);//326, 580, 500 + 326/2, 580/2 - 120 ),
         bottomPanel = createBottomPanel($("#bottom-panel").css({"top":renderHeight - (60 * screenScale) + Math.max(0,(window.innerHeight - renderHeight)/2), "width": renderWidth})),
 
         carouselPanels = [aboutPanel, linksPanel, bioPanel, photosPanel, projectsPanel],
@@ -50,13 +50,13 @@ function main(renderWidth){
 
     /* add add position the main panels */
     scene.add(projectorPanel.quad);
-    // scene.add(subjectPanel.quad);
+    scene.add(subjectPanel.quad);
     scene.add(backgroundPanel.quad);
 
     skeletonPanel.setPosition(350 * screenScale, renderHeight - 20 * screenScale, 1);
     // namePanel.setPosition(50 * screenScale, 358*screenScale, 1);
     // sharePanel.setPosition(20 * screenScale, renderHeight - 20 * screenScale, 1);
-    // subjectPanel.setPosition(500 * screenScale, 450 * screenScale, 1);
+    subjectPanel.setPosition(500 * screenScale, 450 * screenScale, 1);
 
     sharePanel.setPosition(renderWidth + 1000, 0, 0);
     // put the carouselPanels off the right side of the screen
@@ -267,7 +267,7 @@ function main(renderWidth){
         }
 
         projectorPanel.render(time);
-        // subjectPanel.render();
+        subjectPanel.render();
 
         renderer.render(scene, camera);
 
