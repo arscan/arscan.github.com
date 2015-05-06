ab0a0465e7ef9b3c4ac66a15e3afffdbacd3acef
Rob Scanlon <robscanlon@gmail.com>
2015-05-05 22:29:33 -0400

    Updates to tool

--- about/js/ToolPanel.js
+++ about/js/ToolPanel.js
@@ -10,74 +10,213 @@ function createToolPanel(renderer, scale){
 
    var toolBGPlane;
 
+   var coverPlane;
+   
+   var started = false;
+
+   var menuPlanes = [];
+
+   var menu = [
+           ["Languages", ["Javascript", "C#", "Java", "Go"]],
+           ["Editors", ["Vim", "IntelliJ", "Visual Studio"]],
+           ["Platforms", ["Modern Web", "Android"]],
+           ["OS", ["Linux", "Windows"]]
+       ];
+
+   function createBackground(){
+
+        var material = new THREE.MeshBasicMaterial({transparent: false, color: 0x000000});
+        var geometry = new THREE.PlaneBufferGeometry( width, height);
+
+        var plane = new THREE.Mesh( geometry, material );
+        plane.position.set(width/2, height/2, -1);
+        panel.addToScene( plane );
+   };
+
+   function createCoverPlane(){
+
+        var material = new THREE.MeshBasicMaterial({transparent: false, color: 0x000000});
+        var geometry = new THREE.PlaneBufferGeometry( 256 * scale, 100 * scale);
+
+        var plane = new THREE.Mesh( geometry, material );
+        plane.position.set(200 * scale, 110 * scale,2);
+        panel.addToScene( plane );
+        
+        return plane;
+
+   }
+
+   function createTextPlane(text, header, highlighted){
+
+       var titleCanvas =  panel.renderToCanvas(512, 160, function(ctx){
+           ctx.strokeStyle="#eac7df";
+
+           ctx.font = "18pt Roboto";
+           if(header){
+               ctx.font = "30pt Roboto";
+           }
+           // ctx.fillStyle = '#ff8d07';
+           ctx.fillStyle = '#eac7df';
+
+           if(highlighted){
+               ctx.fillStyle = '#eac7df';
+           }
+           ctx.fillText(text, 50, 35);
+
+       });
+
+        var titleTexture = new THREE.Texture(titleCanvas)
+        titleTexture.needsUpdate = true;
+
+        var titleMaterial = new THREE.MeshBasicMaterial({map: titleTexture, transparent: true});
+        var titleGeometry = new THREE.PlaneBufferGeometry( 256 * scale, 80 * scale);
+
+        var plane = new THREE.Mesh( titleGeometry, titleMaterial );
+        return plane;
+
+   };
+
     function init(){
+        createBackground();
         var toolTexture = THREE.ImageUtils.loadTexture('images/tools-foreground.png', undefined, LOADSYNC.register() );
         var toolMaterial = new THREE.MeshBasicMaterial({map: toolTexture, transparent: true});
-        var toolGeometry = new THREE.PlaneBufferGeometry( 512, 512);
+        var toolGeometry = new THREE.PlaneBufferGeometry( 350 * scale, 350 * scale);
         toolPlane = new THREE.Mesh( toolGeometry, toolMaterial );
-        toolPlane.position.set(width/2 - 30, height/2, 1);
+        toolPlane.position.set(width/2 + 45 * scale, height/2 - 40 * scale, 3);
         toolPlane.scale.set(.6,.6,.6);
 
         panel.addToScene( toolPlane );
 
         var toolBGTexture = THREE.ImageUtils.loadTexture('images/tools-background.png', undefined, LOADSYNC.register() );
-        var toolBGMaterial = new THREE.MeshBasicMaterial({map: toolBGTexture, transparent: true, opacity: .5});
-        var toolBGGeometry = new THREE.PlaneBufferGeometry( 512, 512);
+        var toolBGMaterial = new THREE.MeshBasicMaterial({map: toolBGTexture, transparent: true, opacity: .9});
+        var toolBGGeometry = new THREE.PlaneBufferGeometry( 350 * scale, 350 * scale);
         toolBGPlane = new THREE.Mesh( toolBGGeometry, toolBGMaterial );
-        toolBGPlane.position.set(width/2 - 30, height/2, 0);
+        toolBGPlane.position.set(width/2 + 45 * scale, height/2 - 40 * scale, 2);
         toolBGPlane.scale.set(.6,.6,.6);
 
         panel.addToScene( toolBGPlane );
 
-        // var titleCanvas= createTitleCanvas(); 
-        // var titleTexture = new THREE.Texture(titleCanvas)
-        // titleTexture.needsUpdate = true;
+        var headerTexture = THREE.ImageUtils.loadTexture("images/tools-header.png", undefined, LOADSYNC.register() );
+        var headerMaterial = new THREE.MeshBasicMaterial({map: headerTexture, depthTest: false, transparent: true});
+        var headerGeometry = new THREE.PlaneBufferGeometry( 134 * scale, 32 * scale);
+        var headerPlane = new THREE.Mesh(headerGeometry, headerMaterial );
+        headerPlane.position.set(110 * scale, height - 100 * scale,5);
+        headerPlane.scale.set(.7,.7,.7);
+        panel.addToScene(headerPlane);
+        
+        var selectorTexture = THREE.ImageUtils.loadTexture("images/tools-selector.png", undefined, LOADSYNC.register() );
+        var selectorMaterial = new THREE.MeshBasicMaterial({map: selectorTexture, depthTest: false, transparent: true});
+        var selectorGeometry = new THREE.PlaneBufferGeometry( 310 * scale, 200 * scale);
+        var selectorPlane = new THREE.Mesh(selectorGeometry, selectorMaterial );
+        selectorPlane.position.set(160 * scale, height/2 - 32 * scale,5);
+        selectorPlane.scale.set(.5, .5, .5);
+        panel.addToScene(selectorPlane);
 
-        // var titleMaterial = new THREE.MeshBasicMaterial({map: titleTexture, transparent: true});
-        // var titleGeometry = new THREE.PlaneBufferGeometry( 350 * scale, 109 * scale );
 
-        // var plane = new THREE.Mesh( titleGeometry, titleMaterial );
-        // plane.position.set(width/2 + 7, height-60*scale, 0);
-        // panel.addToScene( plane );
+        for(var i =0; i< menu.length; i++){
+            var title = createTextPlane(menu[i][0], true);
+            title.position.set(( 65 + 256/2) * scale, 60 * scale,0);
+            if(i == 0){
+                title.position.x = -1000;
+                console.log("COUND DSF");
+            }
+            panel.addToScene(title);
 
-        // var bodyCanvas= createBodyCanvas(); 
-        // var bodyTexture = new THREE.Texture(bodyCanvas)
-        // bodyTexture.needsUpdate = true;
 
-        // var bodyMaterial = new THREE.MeshBasicMaterial({map: bodyTexture, transparent: true});
-        // var bodyGeometry = new THREE.PlaneBufferGeometry( 350 * scale, 276 * scale );
+            menuPlanes[i] = [];
+            menuPlanes[i].push(title);
+            for(var j = 0; j < menu[i][1].length; j++){
+                var unhighlightedTitle = createTextPlane(menu[i][1][j], false);
+                unhighlightedTitle.position.set(( 65 + 256/2) * scale, 88 * scale + 20*j,0);
 
-        // bodyPlane = new THREE.Mesh( bodyGeometry, bodyMaterial );
-        // bodyPlane.position.set(width/2 + 7, height - 33*scale - (276*scale)/2, 0);
-        // panel.addToScene( bodyPlane );
+                if(i == 0){
+                    unhighlightedTitle.position.x = -1000;
+                }
 
-        // var bottomCanvas= createBottomCanvas(); 
-        // var bottomTexture = new THREE.Texture(bottomCanvas)
-        // bottomTexture.needsUpdate = true;
+                panel.addToScene(unhighlightedTitle);
 
-        // var bottomMaterial = new THREE.MeshBasicMaterial({map: bottomTexture, transparent: true});
-        // var bottomGeometry = new THREE.PlaneBufferGeometry( 350 * scale, 130 * scale );
+                menuPlanes[i].push(unhighlightedTitle);
+            }
+        }
 
-        // bottomPlane = new THREE.Mesh( bottomGeometry, bottomMaterial );
-        // bottomPlane.position.set(185 * scale, 70 * scale, 0);
-        // panel.addToScene( bottomPlane );
+        coverPlane = createCoverPlane();
 
-        // var scrollCanvas= createScrollCanvas(); 
-        // var scrollTexture = new THREE.Texture(scrollCanvas)
-        // scrollTexture.needsUpdate = true;
-
-        // var scrollMaterial = new THREE.MeshBasicMaterial({map: scrollTexture, transparent: true});
-        // var scrollGeometry = new THREE.PlaneBufferGeometry( 256, 25 );
-
-        // scrollPlane = new THREE.Mesh( scrollGeometry, scrollMaterial );
-        // scrollPlane.position.set(0, -100, 0);
-        // panel.addToScene( scrollPlane );
+        console.log(menuPlanes);
 
     }
 
+
     function render(time){
-        toolPlane.rotation.z = time/2;
+        if(!started){
+
+            var rotation = [];
+            var sliders = [];
+            var previous = "";
+
+            var unpackedMenu = [];
+
+            for(var i = 0; i< menu.length; i++){
+                for(var j = 0; j < menu[i][1].length; j++){
+                    unpackedMenu.push([menu[i][0], menu[i][1][j], i]);
+                }
+            }
+
+            for(var i = 0; i< unpackedMenu.length; i++){
+                rotation.push(new TWEEN.Tween(toolPlane.rotation)
+                   .delay(1000)
+                   .easing(TWEEN.Easing.Cubic.InOut)
+                   .to({z: (2 * (i + 1) * Math.PI / unpackedMenu.length ) % (1.9999 * Math.PI)}, 500));
+
+                if(previous != unpackedMenu[i][0]){
+                    sliders.push(new TWEEN.Tween(coverPlane.position)
+                            .to({x:500 * scale}, 750));
+                    sliders.push(new TWEEN.Tween(coverPlane.position)
+                            .delay(menu[(sliders.length-1) / 2][1].length * 1500 - 1500)
+                            .onComplete(function(index){ return function(){
+                                console.log("DO SOME SWITCHING to " + index);
+                                for(var layer = 0; layer<menuPlanes.length; layer++){ 
+                                    for(var layerSub = 0; layerSub < menuPlanes[layer].length; layerSub++){
+                                        if(layer === index){
+                                            menuPlanes[layer][layerSub].position.x = (65 + 256/2) * scale;
+                                        } else {
+                                            menuPlanes[layer][layerSub].position.x = -1000;
+                                        }
+
+                                    }
+
+                                }
+                            }}((unpackedMenu[i][2] + 1) % menu.length))
+                            .to({x:200 * scale}, 750));
+                    if(sliders.length > 2){
+                        sliders[sliders.length - 3].chain(sliders[sliders.length - 2]);
+                    }
+
+                    sliders[sliders.length - 2].chain(sliders[sliders.length - 1]);
+
+                    previous = unpackedMenu[i][0];
+                }
+
+
+                if(i > 0){
+                    rotation[i - 1].chain(rotation[i]);
+                }
+            };
+
+            // rotation.push(new TWEEN.Tween(toolPlane.rotation).delay(1000).easing(TWEEN.Easing.Cubic.InOut).to({z:0}, 500));
+            // rotation[rotation.length - 2].chain(rotation[rotation.length - 1]);
+            rotation[rotation.length - 1].chain(rotation[0]);
+
+            rotation[0].start();
+
+            sliders[sliders.length - 1].chain(sliders[0]);
+            sliders[0].start();
+
+            started = true;
+
+        }
+
         panel.render(time);
+
     }
 
     init();
